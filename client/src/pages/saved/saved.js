30e61edc7c93ca2f42c6d1e680c67c5dcf63a7f4
Nema Darban <nema.darban@gmail.com>
2018-04-20 14:38:41 -0700

    trying async funciton for state

--- client/src/pages/saved/saved.js
+++ client/src/pages/saved/saved.js
@@ -5,10 +5,9 @@ class Saved extends Component {
 
 	state = {
 		posts: [],
-		cats: false,
 	};
 
-	componentDidMount() {
+	componentWillMount() {
 		this.getSaved()
 	};
 
@@ -18,29 +17,31 @@ class Saved extends Component {
 		})
 	}
 
-	getSaved = () => {
-		API.getSavedArticle()
-			.then(res => 
-				this.setState({saved: res.data}, () => console.log('doggy'))
-			)
-			.catch(err => console.log(err))
-			console.log(this.state)
-	};
+	getSaved = async () => {
+		try {
+			/*const savedArticles = await API.getSavedArticles()*/
+			this.setState({saved: (await API.getSavedArticles)})
+		} catch (err) {
+			console.log(err)
+		}
+		console.log(this.state)
+	}
 
 	render () {
 		return (
 			<div className="container">
 			{this.state.posts.length < 0 ? (
 				this.state.posts.map(post => {
-					return (<div key={post._id} className="card article-card">
-								<img src={post.image} alt="" className="card-img-top"></img>
-								<div className="card-body">
-									<a href={post.url} target="_blank"><p className="card-title">{post.title}</p></a>
-									<span className="note-wrapper"><a href="" id={post._id} className="delete btn">Delete</a>
-									<a href="" id={post._id} class="note btn">{this.post.notes.length} Notes</a></span>
-								</div>
+					return (
+						<div key={post._id} className="card article-card">
+							<img src={post.image} alt="" className="card-img-top"></img>
+							<div className="card-body">
+								<a href={post.url} target="_blank"><p className="card-title">{post.title}</p></a>
+								<span className="note-wrapper"><a href="" id={post._id} className="delete btn">Delete</a>
+								<a href="" id={post._id} class="note btn">{this.post.notes.length} Notes</a></span>
 							</div>
-						/*	{{>modal}}*/)
+						</div>
+					/*	{{>modal}}*/)
 				})
 			) : (
 				<div className="banner saved-banner">
