f2325f290e14a078e6bf898242b5a93dfc0a4e67
Nema Darban <nema.darban@gmail.com>
2018-04-20 15:57:38 -0700

    fixing state async

--- client/src/pages/saved/saved.js
+++ client/src/pages/saved/saved.js
@@ -5,40 +5,41 @@ class Saved extends Component {
 
 	state = {
 		posts: [],
+		saved: [],
 	};
 
 	componentWillMount() {
 		this.getSaved()
 	};
 
-	setStateAsync(state) {
+/*	setStateAsync(state) {
 		return new Promise((resolve) => {
 			this.setState(state, resolve)
 		})
-	}
+	}*/
 
 	getSaved = async () => {
 		try {
-			/*const savedArticles = await API.getSavedArticles()*/
-			this.setState({saved: (await API.getSavedArticles)})
+			const savedArticles = await API.getSavedArticle()
+			this.setState({saved: savedArticles.data})
 		} catch (err) {
 			console.log(err)
 		}
-		console.log(this.state)
+		console.log(this.state.saved.length)
 	}
 
 	render () {
 		return (
 			<div className="container">
-			{this.state.posts.length < 0 ? (
-				this.state.posts.map(post => {
+			{this.state.saved.length > 0 ? (
+				this.state.saved.map(post => {
 					return (
 						<div key={post._id} className="card article-card">
 							<img src={post.image} alt="" className="card-img-top"></img>
 							<div className="card-body">
 								<a href={post.url} target="_blank"><p className="card-title">{post.title}</p></a>
 								<span className="note-wrapper"><a href="" id={post._id} className="delete btn">Delete</a>
-								<a href="" id={post._id} class="note btn">{this.post.notes.length} Notes</a></span>
+								<a href="" id={post._id} class="note btn">{this.post.notes.length < 0 ? this.post.notes.length : 0} Notes</a></span>
 							</div>
 						</div>
 					/*	{{>modal}}*/)
